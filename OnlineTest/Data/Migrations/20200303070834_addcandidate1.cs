e88136e1a5ebe6188ba8857b2f22cdbf25f2674c
CC-SR <CC-SR@DESKTOP-JA1OAJF>
2020-03-03 12:36:35 +0500

    yes

---
+++ OnlineTest/Data/Migrations/20200303070834_addcandidate1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineTest.Data.Migrations
+{
+    public partial class addcandidate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "candidates",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Education = table.Column<string>(nullable: true),
+                    Experience = table.Column<string>(nullable: true),
+                    applicationUserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_candidates", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_candidates_AspNetUsers_applicationUserId",
+                        column: x => x.applicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_candidates_applicationUserId",
+                table: "candidates",
+                column: "applicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "candidates");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ OnlineTest/Models/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTest.Models
+{
+    public class Candidate
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Education { get; set; }
+        public string Experience { get; set; }
+        public ApplicationUser applicationUser { get; set; }
+    }
+}
